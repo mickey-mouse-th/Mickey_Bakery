1a1a45509c9b27965c2e8f3286376b4ef29819af
Nattawat Siripratum <nattawat.s@eunit.com>
2025-12-04 20:22:39 +0700

    re-structure

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect PostgreSQL Render
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+// API test
+app.get("/api/test", async (req, res) => {
+  const rows = await pool.query("SELECT NOW()");
+  res.json(rows.rows);
+});
+
+// Serve frontend
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
+
+// Render uses PORT env
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
